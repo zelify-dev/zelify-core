40c5eaf904268b83a3b46097c4f7b0ef2f27591f
Juan Proaño <juan.proano02@epn.edu.ec>
2026-04-20 09:52:07 -0500

    chore: sandbox banner

--- src/modules/customers/components/sandbox-banner.tsx
+++ src/modules/customers/components/sandbox-banner.tsx
@@ -6,20 +6,20 @@ import { XStack, Text } from "tamagui";
 import { useI18n } from "@/providers/i18n-provider";
 
 export const SandboxBanner: React.FC = () => {
-  const { t } = useI18n();
+  // const { t } = useI18n();
 
-  return (
-    <XStack
-      backgroundColor="#0D47A1"
-      paddingVertical="$2"
-      paddingHorizontal="$4"
-      justifyContent="center"
-      alignItems="center"
-      width="100%"
-    >
-      <Text color="white" fontSize="$2" fontWeight="700" letterSpacing={1.2}>
-        {t("common.sandboxEnvironment")}
-      </Text>
-    </XStack>
-  );
+  // return (
+  //   // <XStack
+  //   //   backgroundColor="#0D47A1"
+  //   //   paddingVertical="$2"
+  //   //   paddingHorizontal="$4"
+  //   //   justifyContent="center"
+  //   //   alignItems="center"
+  //   //   width="100%"
+  //   // >
+  //     {/* <Text color="white" fontSize="$2" fontWeight="700" letterSpacing={1.2}>
+  //       {t("common.sandboxEnvironment")}
+  //     </Text> */}
+  //   </XStack>
+  // );
 };
